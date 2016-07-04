183035d3428403c4e25525c2f961c9765de19fd4
Yong Liu <ly@LY-MBPR.local>
2016-07-04 16:42:52 +0800

    Use JAVA_OPTS to specify truststore , keystore

--- src/main/java/net/frontnode/openapi/YingmiApiClient.java
+++ src/main/java/net/frontnode/openapi/YingmiApiClient.java
@@ -3,8 +3,10 @@ package net.frontnode.openapi;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+
 import net.frontnode.openapi.model.Account;
 import net.frontnode.openapi.model.FundSearchInfo;
+
 import org.apache.commons.cli.*;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.HttpResponse;
@@ -20,15 +22,20 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
 import java.io.*;
 import java.net.*;
 import java.security.*;
+import java.security.KeyStore.LoadStoreParameter;
 import java.security.cert.CertificateException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -43,46 +50,59 @@ public class YingmiApiClient {
 
     private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 
-    private String host = "api-test.frontnode.net";
+    private String host;
 
     private String apiKey;
 
     private String apiSecret;
 
     private HttpClient httpClient;
+    
+    final private String keyStoreType;
+    final private String keyStorePath;
+    final private String keyStorePassword;
 
-    private String keyStorePath;
-
-    private String keyStorePassword;
-
-    private String trustStorePath;
-
-    private String trustStorePassword;
+//    private String trustStorePath;
+//    private String trustStorePassword;
 
     private Logger logger = LoggerFactory.getLogger(YingmiApiClient.class);
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    public YingmiApiClient(String host, String apiKey, String apiSecret, String keyStorePath, String keyStorePassword,
-                           String trustStorePath, String trustStorePassword) {
-        if (host != null) this.host = host;
+    // a handy simplified version
+    public YingmiApiClient(String host, String apiKey, String apiSecret ) {
+    	this(
+    			host,
+    			apiKey,
+    			apiSecret,
+    			System.getProperty("javax.net.ssl.keyStoreType", "jks"),
+    			System.getProperty("javax.net.ssl.keyStore"),
+    			System.getProperty("javax.net.ssl.keyStorePassword")
+    	);
+    }
+
+	public YingmiApiClient(String host, String apiKey, String apiSecret, String keyStoreType, String keyStorePath, String keyStorePassword ) {
+        this.host = host;
         this.apiKey = apiKey;
         this.apiSecret = apiSecret;
+        this.keyStoreType = keyStoreType;
         this.keyStorePath = keyStorePath;
         this.keyStorePassword = keyStorePassword;
-        this.trustStorePath = trustStorePath;
-        this.trustStorePassword = trustStorePassword;
 
-        // load key store
         try {
 
-            SSLContext context = SSLContexts.custom()
-                    .loadKeyMaterial(new File(this.keyStorePath),
-                            this.keyStorePassword.toCharArray(),
-                            this.keyStorePassword.toCharArray())
-                    .loadTrustMaterial(new File(this.trustStorePath),
-                            this.trustStorePassword.toCharArray())
-                    .build();
+            // load client certificate
+        	KeyStore ks = KeyStore.getInstance(this.keyStoreType);
+        	ks.load( new FileInputStream( new File( this.keyStorePath)), this.keyStorePassword.toCharArray());
+
+        	// Not calling loadTrustStore() in order to use the settings in $JAVA_OPTS implicitly
+        	// -Djavax.net.ssl.trustStore=/path/to/app-trust-store
+        	// -Djavax.net.ssl.trustStorePassword=password
+        	
+        	SSLContext context = SSLContexts.custom()
+        			.loadKeyMaterial(ks, this.keyStorePassword.toCharArray())
+        			.build();
+        
             SSLConnectionSocketFactory sf = new SSLConnectionSocketFactory(
                     context,
                     new String[] {"TLSv1.2"},
@@ -91,7 +111,14 @@ public class YingmiApiClient {
 
             httpClient = HttpClients.custom().setSSLSocketFactory(sf).build();
 
-        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException | UnrecoverableKeyException | KeyManagementException e) {
+        } catch (
+        		KeyStoreException |
+        		NoSuchAlgorithmException |
+        		CertificateException |
+        		IOException |
+        		UnrecoverableKeyException |
+        		KeyManagementException 
+        		e) {
             e.printStackTrace();
         }
     }
